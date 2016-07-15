41e4febce4c8af3729c1e1209555f25c4aa3b90a
lgabrielcor@gmail.com <luchocorre0716*>
2016-07-15 15:24:39 -0500

    Initial commit

---
+++ app/src/main/java/com/cor/luis/administrator/pruebaandroid/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.cor.luis.administrator.pruebaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
